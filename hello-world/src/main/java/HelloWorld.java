158825d8c79abb893472c1fe73093c868857be74
dayFun <joethorngren@gmail.com>
2016-06-24 21:28:14 -0500

    Hello World exercise complete.

---
+++ hello-world/src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static String hello(String name) {
+        String hello = "Hello, ";
+        if (name == null || name.equals("")) {
+            name = "World";
+        }
+
+        return hello + name + "!";
+    }
+
+
+}
