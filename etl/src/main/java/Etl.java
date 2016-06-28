f156e6333d4fdbb700057aa9ec40db957fe72f6c
dayFun <joethorngren@gmail.com>
2016-06-27 22:16:52 -0500

    ETL solution using RxJava.

--- etl/src/main/java/Etl.java
+++ etl/src/main/java/Etl.java
@@ -1,18 +1,20 @@
-import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import rx.Observable;
+import rx.functions.Func1;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toMap;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Etl {
 
     public Map<String, Integer> transform(Map<Integer, List<String>> old) {
         Map<String, Integer> tileValues = new HashMap<>();
 
-        old.forEach((value, letters) -> letters.stream()
-                                               .forEach(tile -> tileValues.put(tile.toLowerCase(), value)));
+        Observable.from(old.entrySet()).subscribe(entry -> {
+            entry.getValue().forEach(letter -> {
+                tileValues.put(letter.toLowerCase(), entry.getKey());
+            });
+        });
 
         return tileValues;
     }
