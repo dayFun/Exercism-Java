0101b8f7ca513145c2c6cfd23bc0fb570ad9b7ff
dayFun <joethorngren@gmail.com>
2016-06-28 22:47:58 -0500

    Working implementation for #3. Need to refactor.

--- nucleotide-count/src/main/java/DNA.java
+++ nucleotide-count/src/main/java/DNA.java
@@ -1,2 +1,52 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DNA {
+
+    private static final char ADENINE = 'A';
+    private static final char CYTOSINE = 'C';
+    private static final char GUANINE = 'G';
+    private static final char THYMINE = 'T';
+
+    private static final List<Character> NUCLEOTIDES = Arrays.asList(ADENINE, CYTOSINE, GUANINE, THYMINE);
+
+    private final String dnaString;
+
+    public DNA(final String dnaString) {
+        this.dnaString = dnaString;
+    }
+
+    public Map<Character, Integer> nucleotideCounts() {
+        Map<Character, Integer> dnaMap = initDnaMap();
+
+        NUCLEOTIDES.forEach(nucleotide -> dnaMap.put(nucleotide, count(nucleotide)));
+
+        return dnaMap;
+    }
+
+    public int count(char nucleotide) {
+        if (!NUCLEOTIDES.contains(nucleotide)) {
+            throw new IllegalArgumentException();
+        }
+
+        int total = 0;
+        for (int i = 0; i < dnaString.length(); i++) {
+            if (dnaString.charAt(i) == nucleotide) {
+                total++;
+            }
+        }
+        return total;
+    }
+
+    private Map<Character, Integer> initDnaMap() {
+        Map<Character, Integer> dnaMap = new HashMap<>();
+        dnaMap.put(ADENINE, 0);
+        dnaMap.put(CYTOSINE, 0);
+        dnaMap.put(GUANINE, 0);
+        dnaMap.put(THYMINE, 0);
+
+        return dnaMap;
+    }
 }
